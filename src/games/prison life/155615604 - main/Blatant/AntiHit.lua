5e542243685a32fe3936f3d991dad5588636deef
badardfday <barfing.lmao1@gmail.com>
2026-09-17 23:32:29 +0800

    rythm of the night

--- src/games/prison life/155615604 - main/Blatant/AntiHit.lua
+++ src/games/prison life/155615604 - main/Blatant/AntiHit.lua
@@ -3,35 +3,15 @@ local Intensity
 local oldCF
 local oldRoot
 local oldFallenHeight
-local renderStepKey = 'AntiHit_' .. tostring(math.random(100000, 999999))
 
 AntiHit = vape.Categories.Blatant:CreateModule({
     Name = 'AntiHit',
     Function = function(callback)
         if callback then
-            oldCF = nil
             oldFallenHeight = workspace.FallenPartsDestroyHeight
             workspace.FallenPartsDestroyHeight = -math.huge
 
-            runService:BindToRenderStep(renderStepKey, Enum.RenderPriority.Camera.Value - 1, function()
-                if entitylib.isAlive then
-                    local root = entitylib.character.RootPart
-                    if root ~= oldRoot then
-                        oldCF = nil
-                        oldRoot = root
-                    end
-                end
-                if entitylib.isAlive and oldCF and entitylib.character.RootPart == oldRoot then
-                    local root = entitylib.character.RootPart
-                    root.CFrame = oldCF
-                end
-            end)
-
-            AntiHit:Clean(function()
-                runService:UnbindFromRenderStep(renderStepKey)
-            end)
-
-            AntiHit:Clean(runService.PreSimulation:Connect(function()
+            AntiHit:Clean(runService.PostSimulation:Connect(function()
                 if entitylib.isAlive then
                     local root = entitylib.character.RootPart
                     oldRoot = root
@@ -42,14 +22,8 @@ AntiHit = vape.Categories.Blatant:CreateModule({
                     root.CFrame += offset
 
                     if root.Position.Y > 179.99 then
-                        root.CFrame -= Vector3.new(0, root.Position.Y - 179.99, 0)
+                        root.CFrame = root.CFrame - Vector3.new(0, root.Position.Y - 179.99, 0)
                     end
-                end
-            end))
-
-            AntiHit:Clean(runService.PostSimulation:Connect(function()
-                if entitylib.isAlive and oldCF and entitylib.character.RootPart == oldRoot then
-                    entitylib.character.RootPart.CFrame = oldCF
                 end
             end))
         else
@@ -66,7 +40,7 @@ AntiHit = vape.Categories.Blatant:CreateModule({
             end
         end
     end,
-    Tooltip = 'Offsets your position to make RootPart targeting less reliable'
+    Tooltip = 'Offsets your position without locking movement'
 })
 Intensity = AntiHit:CreateSlider({
     Name = 'Intensity',
